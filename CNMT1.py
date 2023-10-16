11cf92e6627244d182eb80d1bcd4fa34e3f28635
Allis0n95 <146487008+Allis0n95@users.noreply.github.com>
2023-10-16 15:59:09 -0500

    Update CNMT1.py

--- CNMT1.py
+++ CNMT1.py
@@ -1,4 +1,20 @@
+rolls = int(input ('How many times do you want to roll?'))
+sides = int(input ('How many sides is your die?'))
 
+import random
 
-name = input ('Enter your name: ')
-print ('Your name is: ' + name)
+for rolls in range (rolls):
+    randomDice = random.randrange(sides)+1
+    print (randomDice)
+    if randomDice==20:
+        print('critical hit!')
+    if randomDice==1:
+        print('critical FAIL')
+        
+print ('done rolling')
+
+#Link I used for help
+#https://codehs.com/tutorial/rachel/user-input-in-python
+
+#Github link
+#
