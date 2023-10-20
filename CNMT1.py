fd8c7cd94f166e45c40ce1cecddf7a48813f016d
Allis0n95 <146487008+Allis0n95@users.noreply.github.com>
2023-10-20 10:57:30 -0500

    Update CNMT1.py
    also called DiceGame

--- CNMT1.py
+++ CNMT1.py
@@ -1,17 +1,20 @@
-rolls = int(input ('How many times do you want to roll?'))
-sides = int(input ('How many sides is your die?'))
+rolls = int(input ('How many times do you want to roll?')) #asks for number of rolls
+sides = int(input ('How many sides is your die?')) #asks for number of sides
+#above is the roll and side integers
 
-import random
+import random #makes sure the numbers are random
 
-for rolls in range (rolls):
-    randomDice = random.randrange(sides)+1
-    print (randomDice)
+for rolls in range (rolls): #rolls dice based off of roll input number
+    randomDice = random.randrange(sides)+1 #rolls dice/die with amount of sides as side input number
+    print (randomDice) #shows dice/die roll numbers to screen
     if randomDice==20:
-        print('critical hit!')
+        print('critical hit!') #checks for critical hit (20) then outputs if true
     if randomDice==1:
-        print('critical FAIL')
+        print('critical FAIL') #checks for critical fail (1) then outputs if true
         
 print ('done rolling')
+#will say when the code is done and that there will be no more rolls
+
 
 #Link I used for help
 #https://codehs.com/tutorial/rachel/user-input-in-python
